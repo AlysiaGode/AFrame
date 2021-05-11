d90dde2df472688b4f24276e033113a439fda7cd
Alysia <alysiagode@ziggo.nl>
2021-05-11 19:54:36 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,13 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.set(
+  		THREE.Math.degToRad(15),
+  		THREE.Math.degToRad(30),
+  		THREE.Math.degToRad(90)
+	);
+	myOtherBox.object3D.rotation -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
